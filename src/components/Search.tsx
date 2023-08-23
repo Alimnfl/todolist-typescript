ed6df9fa00d3b989b345cb132e9289fe0daf5891
Alimnfl <mnaufallim234@gmail.com>
2023-08-24 00:01:38 +0700

    Add Component Search

---
+++ src/components/Search.tsx
@@ -0,0 +1,22 @@
+import { ChangeEvent } from 'react';
+import { FaSearch } from 'react-icons/fa';
+
+interface SearchProps {
+  setSearchText: (searchText: string) => void;
+}
+
+function Search({ setSearchText }: SearchProps) {
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const searchText = e.target.value;
+    setSearchText(searchText);
+  };
+  return (
+    <div className="flex flex-row items-center w-full gap-2 p-2 mt-6 border rounded-md bg-slate-200">
+      <FaSearch size="12px" classname="text-white" />
+      <input type="text" placeholder="Search your note" onChange={handleChange} className="bg-slate-200 w-full h-full text-[14px]" />
+    </div>
+    //can search with handleChange
+  );
+}
+
+export default Search;
