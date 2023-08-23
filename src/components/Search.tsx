e9db72aaed93d4b11925f01fa82985febf1801b0
Alimnfl <mnaufallim234@gmail.com>
2023-08-24 00:22:27 +0700

    Updated Search

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -12,7 +12,7 @@ function Search({ setSearchText }: SearchProps) {
   };
   return (
     <div className="flex flex-row items-center w-full gap-2 p-2 mt-6 border rounded-md bg-slate-200">
-      <FaSearch size="12px" classname="text-white" />
+      <FaSearch size="12px" className="text-white" />
       <input type="text" placeholder="Search your note" onChange={handleChange} className="bg-slate-200 w-full h-full text-[14px]" />
     </div>
     //can search with handleChange
