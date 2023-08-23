0a6cf73fb622e3ebb29c65ee32037912c41fb647
Alimnfl <mnaufallim234@gmail.com>
2023-08-24 00:01:28 +0700

    Add Component Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <div className="w-full flex justify-between  items-center">
+      <h1 className="text-2xl font-bold">Alimnfl Todos - React-TS</h1>
+      <button className="rounded-xl p-1 bg-slate-300 font-medium text-md px-3">Toggle Mode</button>
+    </div>
+  );
+}
+
+export default Header;
