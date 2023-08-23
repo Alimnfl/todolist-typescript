b324ea3502480c791a0e84d91138cfb163cd0b7d
Alimnfl <mnaufallim234@gmail.com>
2023-08-24 00:00:16 +0700

    Add Component TodoList

---
+++ src/components/TodosList.tsx
@@ -0,0 +1,31 @@
+import AddTodos from './AddTodos';
+import Todo from './Todos';
+
+interface Todo {
+  id: string;
+  text: string;
+  date: string;
+}
+
+// interface AddTodosProps {
+//   handleAddTodo: (addTodo: string) => void;
+// }
+
+interface TodoProps {
+  todos: Todo[];
+  handleDeleteTodo: (deleteTodo: string) => void;
+  handleAddTodo: (addTodo: string) => void;
+}
+
+function TodosList({ todos, handleDeleteTodo, handleAddTodo }: TodoProps) {
+  return (
+    <div className="w-full grid grid-cols-3 gap-[30px] border border-blue-300">
+      {todos.map((todo) => (
+        <Todo id={todo.id} text={todo.text} date={todo.date} handleDeleteTodo={handleDeleteTodo} />
+      ))}
+      <AddTodos handleAddTodo={handleAddTodo} />
+    </div>
+  );
+}
+
+export default TodosList;
