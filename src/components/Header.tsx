40e7bf6dbf050f8e852a5754c4dfe37b360b5674
Alimnfl <mnaufallim234@gmail.com>
2023-08-24 01:41:17 +0700

    Updated Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,18 @@
-function Header() {
+interface HeaderProps {
+  handleToggleMode: (previousDarkMode: boolean) => void;
+}
+
+function Header({ handleToggleMode }: HeaderProps) {
+  const modeOnScreen = () => {
+    handleToggleMode((previousDarkMode: boolean) => !previousDarkMode);
+  };
+
   return (
-    <div className="w-full flex justify-between  items-center">
-      <h1 className="text-2xl font-bold">Alimnfl Todos - React-TS</h1>
-      <button className="rounded-xl p-1 bg-slate-300 font-medium text-md px-3">Toggle Mode</button>
+    <div className="flex items-center justify-between w-full text-black">
+      <h1 className="text-2xl font-bold ">Alimnfl Todos - React-TS</h1>
+      <button onClick={modeOnScreen} className="p-1 px-3 font-medium rounded-xl bg-slate-300 text-md">
+        Toggle Mode
+      </button>
     </div>
   );
 }
