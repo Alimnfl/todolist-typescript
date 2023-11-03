8593c2c870405d5e3c9112934156c3bf730f63fe
Alimnfl <mnaufallim234@gmail.com>
2023-11-03 07:46:44 +0700

    Updated Header and Todos

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,7 +9,7 @@ function Header({ handleToggleMode }: HeaderProps) {
 
   return (
     <div className="flex items-center justify-between w-full text-black">
-      <h1 className="text-2xl font-bold ">Alimnfl Todos - React-TS</h1>
+      <h1 className="text-2xl font-bold ">Alimnfl Todos</h1>
       <button onClick={modeOnScreen} className="p-1 px-3 font-medium rounded-xl bg-slate-300 text-md">
         Toggle Mode
       </button>
--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -2,20 +2,34 @@ import { FaTrashAlt } from 'react-icons/fa';
 
 interface TodoProps {
   id: string;
-  text: string;
-  date: string;
+  title: string;
+  body: string;
+  archived: boolean;
+  createdAt: string;
   handleDeleteTodo: (id: string) => void;
+  handleArchiveClick: (id: string) => void;
 }
 
-function Todo({ id, text, date, handleDeleteTodo }: TodoProps) {
+function Todo({ id, body, title, archived, createdAt, handleDeleteTodo, handleArchiveClick }: TodoProps) {
   const onDeleteClick = () => {
     handleDeleteTodo(id);
   };
+  const onArchiveClick = () => {
+    handleArchiveClick(id);
+  };
   return (
-    <div className="flex flex-col drop-shadow-md justify-between mt-6 p-4 w-[400px] border h-[200px] bg-yellow-200 rounded-xl">
-      <span className="text-xs">{text}</span>
+    <div key={id} className="flex flex-col drop-shadow-md justify-between mt-6 p-4 w-[400px] border h-[200px] bg-yellow-200 rounded-xl">
+      <div className="flex flex-col gap-2">
+        <span className="text-lg font-semibold ">{title}</span>
+        <span className="text-xs">{body}</span>
+      </div>
       <div className="flex flex-row items-center justify-between">
-        <p className="">{date}</p>
+        <p className="">{createdAt}</p>
+        {!archived ? (
+          <button className="font-semibold" onClick={onArchiveClick}>
+            Archive
+          </button>
+        ) : null}
         <FaTrashAlt onClick={onDeleteClick} className="cursor-pointer" />
       </div>
     </div>
