05b5fb37701f62dae8d35c927845cabecb29a50a
Alimnfl <mnaufallim234@gmail.com>
2023-11-03 07:57:00 +0700

    Updated TodosList

--- src/components/TodosList.tsx
+++ src/components/TodosList.tsx
@@ -1,29 +1,65 @@
+import { useState } from 'react';
 import AddTodos from './AddTodos';
 import Todo from './Todos';
 
 interface Todo {
   id: string;
-  text: string;
-  date: string;
+  title: string;
+  body: string;
+  archived: boolean;
+  createdAt: string;
 }
-
-// interface AddTodosProps {
-//   handleAddTodo: (addTodo: string) => void;
-// }
 
 interface TodoProps {
   todos: Todo[];
   handleDeleteTodo: (deleteTodo: string) => void;
-  handleAddTodo: (addTodo: string) => void;
+  handleAddTodo: (title: string, body: string, boolean: boolean) => void;
 }
 
 function TodosList({ todos, handleDeleteTodo, handleAddTodo }: TodoProps) {
+  const [archivedTodos, setArchivedTodos] = useState<Todo[]>([]);
+
+  const handleArchiveClick = (todoId: string) => {
+    const todoIndex = todos.findIndex((todo) => todo.id === todoId);
+    if (todoIndex !== -1) {
+      const todoArchive = { ...todos[todoIndex] };
+      todoArchive.archived = true;
+
+      const updatedTodos = [...todos];
+      updatedTodos.splice(todoIndex, 1);
+
+      handleDeleteTodo(todoId);
+      setArchivedTodos((prevArchivedTodos) => [...prevArchivedTodos, todoArchive]);
+    }
+  };
+
+  const handleDeleteArchivedTodo = (todoId: string) => {
+    const archivedTodoIndex = archivedTodos.findIndex((todo) => todo.id === todoId);
+    if (archivedTodoIndex !== -1) {
+      const updatedArchivedTodos = [...archivedTodos];
+      updatedArchivedTodos.splice(archivedTodoIndex, 1);
+      setArchivedTodos(updatedArchivedTodos);
+    }
+  };
   return (
-    <div className="w-full grid grid-cols-3 gap-[30px]">
-      {todos.map((todo) => (
-        <Todo id={todo.id} text={todo.text} date={todo.date} handleDeleteTodo={handleDeleteTodo} />
-      ))}
-      <AddTodos handleAddTodo={handleAddTodo} />
+    <div className="flex flex-col py-4">
+      <div className="flex flex-col">
+        <h1 className="text-lg font-semibold">Todo List</h1>
+        <div className="w-full grid grid-cols-3 gap-[30px]">
+          {todos.map((todo) => (
+            <Todo id={todo.id} title={todo.title} body={todo.body} archived={false} createdAt={todo.createdAt} handleDeleteTodo={handleDeleteTodo} handleArchiveClick={() => handleArchiveClick(todo.id)} />
+          ))}
+          <AddTodos handleAddTodo={handleAddTodo} />
+        </div>
+      </div>
+      <div className="pt-4">
+        <h1 className="text-lg font-semibold">Archive</h1>
+        <div className="w-full grid grid-cols-3 gap-[30px]">
+          {archivedTodos.map((todo) => (
+            <Todo id={todo.id} title={todo.title} body={todo.body} archived={false} createdAt={todo.createdAt} handleDeleteTodo={handleDeleteArchivedTodo} handleArchiveClick={() => handleArchiveClick(todo.id)} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
