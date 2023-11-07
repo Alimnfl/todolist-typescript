1d7f54b2e017af0b84d132edb1aca53295d19b69
Alimnfl <mnaufallim234@gmail.com>
2023-11-07 11:09:49 +0700

    Updated Header Tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,12 +4,12 @@ interface HeaderProps {
 
 function Header({ handleToggleMode }: HeaderProps) {
   const modeOnScreen = () => {
-    handleToggleMode((previousDarkMode: boolean) => !previousDarkMode);
+    handleToggleMode((previousDarkmode: boolean) => !previousDarkmode);
   };
 
   return (
     <div className="flex items-center justify-between w-full text-black">
-      <h1 className="text-2xl font-bold ">Alimnfl Todos</h1>
+      <h1 className="text-2xl font-bold">Alimnfl Todos</h1>
       <button onClick={modeOnScreen} className="p-1 px-3 font-medium rounded-xl bg-slate-300 text-md">
         Toggle Mode
       </button>
