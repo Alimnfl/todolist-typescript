cc01326a058e5b041f000bd8dd1edf2cdcbc447c
Alimnfl <mnaufallim234@gmail.com>
2023-11-09 03:32:32 +0700

    Make change on Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,18 @@
-interface HeaderProps {
-  handleToggleMode: (previousDarkMode: boolean) => void;
+import { Dispatch, SetStateAction } from 'react';
+
+interface Header {
+  handleToggleMode: Dispatch<SetStateAction<boolean>>;
 }
 
-function Header({ handleToggleMode }: HeaderProps) {
-  const modeOnScreen = () => {
+function Header({ handleToggleMode }: Header) {
+  const handleToggleDarkMode = () => {
     handleToggleMode((previousDarkmode: boolean) => !previousDarkmode);
   };
 
   return (
     <div className="flex items-center justify-between w-full text-black">
       <h1 className="text-2xl font-bold">Alimnfl Todos</h1>
-      <button onClick={modeOnScreen} className="p-1 px-3 font-medium rounded-xl bg-slate-300 text-md">
+      <button onClick={handleToggleDarkMode} className="p-1 px-3 font-medium rounded-xl bg-slate-300 text-md">
         Toggle Mode
       </button>
     </div>
