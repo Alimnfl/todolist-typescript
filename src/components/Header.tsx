53f3da1538206416fcb6e9bb545ce06558f61a8f
Alimnfl <mnaufallim234@gmail.com>
2023-11-09 04:15:13 +0700

    Updated Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,18 @@
 import { Dispatch, SetStateAction } from 'react';
 
-interface Header {
+interface HeaderProps {
   handleToggleMode: Dispatch<SetStateAction<boolean>>;
+  toggleMode: boolean;
 }
 
-function Header({ handleToggleMode }: Header) {
+function Header({ handleToggleMode, toggleMode }: HeaderProps) {
   const handleToggleDarkMode = () => {
     handleToggleMode((previousDarkmode: boolean) => !previousDarkmode);
   };
 
   return (
     <div className="flex items-center justify-between w-full text-black">
-      <h1 className="text-2xl font-bold">Alimnfl Todos</h1>
+      <h1 className={`text-2xl font-bold ${toggleMode ? 'text-white' : 'text-black'}`}>Alimnfl Todos</h1>
       <button onClick={handleToggleDarkMode} className="p-1 px-3 font-medium rounded-xl bg-slate-300 text-md">
         Toggle Mode
       </button>
